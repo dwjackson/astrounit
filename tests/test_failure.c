03c93b0c2c5f5bfbff659de660283a23dc279aa8
David Jackson <david@dwjackson.ca>
2017-09-02 11:22:53 -0400

    Add a test for failures

---
+++ tests/test_failure.c
@@ -0,0 +1,25 @@
+#include "astrounit.h"
+#include <stdlib.h>
+
+ASTRO_TEST_BEGIN(test_str_eq)
+{
+	char str1[] = "test";
+	char str2[] = "test2";
+	assert_str_eq(str1, str2, "strings are equal (they should not be)");
+}
+ASTRO_TEST_END
+
+int
+main(void)
+{
+	int num_failures;
+	struct astro_suite *suite;
+
+	suite = astro_suite_create();
+	astro_suite_add_test(suite, test_str_eq, NULL);
+	astro_suite_run(suite);
+	num_failures = astro_suite_num_failures(suite);
+	astro_suite_destroy(suite);
+
+	return (num_failures == 1 ? EXIT_SUCCESS : EXIT_FAILURE);
+}
